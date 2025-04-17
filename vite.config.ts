69e15c96954ebd6674cffab73c26da68d60fecd9
vkkarthik17 <vkkarthik488@gmail.com>
2025-04-17 12:26:54 +0530

    Setup GitHub Pages deploy for frontend

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: '/Nextgen/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
